0b2ef679f8ad6fff2265f70d9f0758d43b78ae9e
Saleh-Abdulaziz-Alobadi <158226172+Saleh-Abdulaziz-Alobadi@users.noreply.github.com>
2024-01-31 01:36:16 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:test_1/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+
+    );
+  }
+}
+
