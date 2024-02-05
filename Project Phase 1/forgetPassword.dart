ddf529ad9fb5faea31e5ce6a77839687f04b35dd
Saleh-Abdulaziz-Alobadi <158226172+Saleh-Abdulaziz-Alobadi@users.noreply.github.com>
2024-02-05 18:42:06 +0300

    Add files via upload

---
+++ Project Phase 1/forgetPassword.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  State<ForgetPassword> createState() => _ForgetPasswordState();
+}
+
+class _ForgetPasswordState extends State<ForgetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Forget Password'),
+      ),
+    );
+  }
+}
+
---
+++ Project Phase 1/login_page.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:test_1/forgetPassword.dart';
+import 'package:test_1/signUp.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginSignupPageState();
+}
+
+class _LoginSignupPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        width: MediaQuery.of(context).size.width,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 424), //how far email field from above
+              TextField(
+                decoration: InputDecoration(
+                    hintText: 'Email',hintStyle: TextStyle(color: Colors.white),
+                    prefixIcon: const Icon(Icons.email),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        // change color of backgroun Email field
+                        width: 1.0,
+                      ),
+                    ),
+                    disabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 1.0,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        color: Colors.blue,
+                        width: 1.0,
+                      ),
+                    )),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+
+                decoration: InputDecoration(
+                  hintText: 'password',hintStyle: TextStyle(color: Colors.white),
+                  prefixIcon: const Icon(Icons.email),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      // change color of backgroun Email
+                      width: 1.0,
+                    ),
+                  ),
+                  disabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 1.0,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(
+                      color: Colors.blue,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 50),
+              MaterialButton(
+                  elevation: 5.0 ,
+                  color: Colors.blue, //backgroun Login
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 20, horizontal: 80),
+                  child: const Text(
+                      'Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                      ),
+                  ),
+                  shape:OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30), // radius of login feld
+                    borderSide: BorderSide.none,
+                  ) ,
+                  onPressed: () {}),
+              const SizedBox(height: 20), //size forget the password
+
+              GestureDetector(
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder:(context){
+                    return const ForgetPassword();
+                  }));
+                },
+                child: Text('Forget the Password ?',
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+
+                ) ),
+              ),
+              const SizedBox(height: 20), //size Sign up
+
+              GestureDetector(
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(builder:(context){
+                    return const SignUp();
+                  }));
+                },
+                child: Text('Sign up',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+
+                    ) ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
